7087f9b2926f47d18a8d8a5e666f384c177c0508
sakout-reda <sakout-reda@hotmail.fr>
2024-03-23 14:10:55 +0000

    initial commit

---
+++ src/main/java/com/example/springbatchessentials/SpringBatchEssentialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchEssentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchEssentialsApplication.class, args);
+    }
+
+}
